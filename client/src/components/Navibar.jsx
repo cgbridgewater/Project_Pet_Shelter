14ce98a8c76facbeecf03dfba7fb8e413d024233
Chris Bridgewater <cgbridgewater@outlook.com>
2023-05-30 23:32:24 -0700

    style changes

--- client/src/components/Navibar.jsx
+++ client/src/components/Navibar.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 const Navibar = () => {
 
   return (
-    <div>
+    <div style={{backgroundColor:"whitesmoke"}}>
       {/* navbar container */}
-      <nav style={{flexWrap:"wrap",height:"auto", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"1%", paddingLeft:"5%", borderBottom:"10px double #073DAA",backgroundImage:"url(https://media.istockphoto.com/id/1276581591/vector/cute-simple-seamless-pattern-with-paw-prints.jpg?s=612x612&w=0&k=20&c=qRxqN8NX0Q0VkLOOj0NCysIZZ37328M4W2Kze2Xofro=)"}}>
+      <nav style={{flexWrap:"wrap",height:"auto", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"1%", paddingLeft:"5%", borderBottom:"10px double #073DAA"}}>
         {/* left side of navbar */}
         <div style={{minWidth:"355px",width:"30%", textAlign:"start"}} className='LeftNav'>
           <Link to="/petshelter" style={{fontSize:"30px", fontWeight:"700"}}>Coding Dojo Pet Shelter</Link>
