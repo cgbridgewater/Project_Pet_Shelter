4d36c98868738443769400d49ff090cd58b40a14
Chris Bridgewater <cgbridgewater@outlook.com>
2023-05-31 01:23:33 -0700

    edit logos and background

--- client/src/components/Navibar.jsx
+++ client/src/components/Navibar.jsx
@@ -3,12 +3,19 @@ import { Link } from "react-router-dom"
 const Navibar = () => {
 
   return (
-    <div style={{backgroundColor:"whitesmoke"}}>
+    <div style={{background:"url('https://media.istockphoto.com/id/1276581591/vector/cute-simple-seamless-pattern-with-paw-prints.jpg?s=612x612&w=0&k=20&c=qRxqN8NX0Q0VkLOOj0NCysIZZ37328M4W2Kze2Xofro=')"}}>
       {/* navbar container */}
       <nav style={{flexWrap:"wrap",height:"auto", display:"flex", justifyContent:"space-between", alignItems:"center", padding:"1%", paddingLeft:"5%", borderBottom:"10px double #073DAA"}}>
         {/* left side of navbar */}
-        <div style={{minWidth:"355px",width:"30%", textAlign:"start"}} className='LeftNav'>
-          <Link to="/petshelter" style={{fontSize:"30px", fontWeight:"700"}}>Coding Dojo Pet Shelter</Link>
+        <div style={{minWidth:"355px",width:"30%", textAlign:"start"}}>
+          <Link to="/petshelter">
+            <h1 style={{fontSize:"26px", fontWeight: 800, lineHeight:"90%", color:"#073DAA"}}>
+              Coding Dojo Pet Shelters
+            </h1>
+            <h4 style={{color:"#073DAA", textAlign:"end"}}>
+              Save one until there are none.
+            </h4>
+          </Link>
         </div>
           <Link className="LiveFeed" to={`/petshelter/livefeed`}></Link>
         {/* right side of navbar */}
