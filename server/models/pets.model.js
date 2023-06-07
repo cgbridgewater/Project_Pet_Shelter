daabb56340a9e018e3f80e39bedf24f6b6a2ac28
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-07 12:59:42 -0700

    update model for pet

--- server/models/pets.model.js
+++ server/models/pets.model.js
@@ -3,38 +3,44 @@ const mongoose = require('mongoose');
 const PetsSchema = new mongoose.Schema({
     name: { 
         type: String,
-        required: [true, "Pet Name required"],
-        minLength: [3, "Must be at Least 3 characters long"],
+        required: [true, "Name required"],
+        minLength: [3, "Name must be at Least 3 characters long"],
+        maxLength: [30,"Name can not exceed 300 characters!"]
     },
     type: { 
         type: String,
-        required: [true, "Pet type required"],
-        minLength: [3, "Pet type must be at least 3 characters long"],
+        required: [true, "Type required"],
+        minLength: [3, "Type must be at least 3 characters long"],
     },
     description: { 
         type: String,
-        required: [true, "Pet description required"],
-        minLength: [3, "Pet description must be at least 3 characters long"],
+        required: [true, "Description required"],
+        minLength: [3, "Description must be at least 3 characters long"],
+        maxLength: [300,"Description can not exceed 300 characters!"]
     },
-    skillOne: { 
-        type: String,
-},
-    skillTwo: { 
-        type: String,
-},
-    skillThree: { 
-},
     age: {
-        type: Number,
+        type:Number,
+        required:[true,"Age is required!"],
+        min:[1,"Age must be at least 1!"],
+        validate:{
+            validator:Number.isInteger,
+            message:"Age must be a whole number!"
+        }
     },
-// likes: {  // added but not used. will be used to contain likes between pages.
-//     type: Number,
-// },
-//     array: { // added but not used. 
-//         type: String,
-//         // uniqueItems: true,
-//         maxItems: [3, "max 3 items"]
-// }
-
+    petImage:{
+        type:String,
+        required:[true,"Pet image link is required!"]
+    },
+    animalFriendly:{
+        type:Boolean,
+        default:false,
+    },
+    childFriendly:{
+        type:Boolean,
+        default:false,
+    },
+    notes: { 
+        type: String,
+    },
 }, { timestamps: true }); 
 module.exports = mongoose.model('Pets', PetsSchema);
