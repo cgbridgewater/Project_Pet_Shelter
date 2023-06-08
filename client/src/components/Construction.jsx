0c9cdceb3605b740b7f631a6ab525db165b68f15
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-07 17:46:13 -0700

    optimization

--- client/src/components/Construction.jsx
+++ client/src/components/Construction.jsx
@@ -5,7 +5,7 @@ const Construction = () => {
     return(
         <div style={{minHeight:"65vh"}}>
             <Link to="/petshelter" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline"}}>Return To Pet Shelter</Link><br />
-            <img style={{margin:"2% auto", height:"457px"}} src="https://png.pngitem.com/pimgs/s/46-464912_site-under-construction-hd-png-download.png" alt="Under Construction" />
+            <img style={{margin:"2% auto", width:"30%", height:"15%", minWidth:"325px", minHeight:"163px"}} src="https://png.pngitem.com/pimgs/s/46-464912_site-under-construction-hd-png-download.png" alt="Under Construction" />
         </div>
     ) 
 }
