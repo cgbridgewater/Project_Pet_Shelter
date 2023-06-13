3efe8943b24b7d05c2beff045d2689f8c2d1654d
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-12 23:38:23 -0700

    remove style link

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
