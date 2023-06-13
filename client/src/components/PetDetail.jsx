d489c0cb897b1e3b6980ae97c29ec641bc2a6e7c
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-12 23:39:57 -0700

    update pet attributes

--- client/src/components/PetDetail.jsx
+++ client/src/components/PetDetail.jsx
@@ -16,10 +16,12 @@ const Detail = () => {
     const [ name, setName ] = useState("");
     const [ type, setType ] = useState("");
     const [ description, setDescription ] = useState("");
-    const [ skillOne, setSkillOne ] = useState("");
-    const [ skillTwo, setSkillTwo ] = useState("");
-    const [ skillThree, setSkillThree ] = useState("");
     const [ age, setAge ] = useState("");
+    const [ petImage, setpetImage ] = useState("");
+    const [ notes, setNotes ] = useState("");
+    const [ fixed, setFixed ] = useState("");
+    const [ childFriendly, setChildFriendly ] = useState("");
+    const [ animalFriendly, setAnimalFriendly ] = useState("");
     const [getErrors, setGetErrors] = useState("null");
 
         useEffect(() => {
@@ -29,10 +31,12 @@ const Detail = () => {
                 setName(res.data.name);
                 setType(res.data.type);
                 setDescription(res.data.description);
-                setSkillOne(res.data.skillOne);
-                setSkillTwo(res.data.skillTwo);
-                setSkillThree(res.data.skillThree);
                 setAge(res.data.age);
+                setpetImage(res.data.petImage);
+                setNotes(res.data.notes);
+                setFixed(res.data.fixed);
+                setChildFriendly(res.data.childFriendly);
+                setAnimalFriendly(res.data.animalFriendly);
                 setGetErrors("")
             })
             .catch((err) => {
@@ -41,20 +45,21 @@ const Detail = () => {
             })
         }, []);
 
-        const adoptPet = (e) => {
-            axios.delete('http://localhost:8000/api/pets/' + id)
-                .then(res => {
-                    alert(`Congratulations!! ${name} has lots of love to give and will make a great pet in your home.`)
-                    navigate("/petshelter");
-                })
-                .catch((err) => {
-                    console.log(err.response.data)
-                    setGetErrors(err.response.data); 
-                })
-        }
+        // const adoptPet = (e) => {
+        //     axios.delete('http://localhost:8000/api/pets/' + id)
+        //         .then(res => {
+        //             alert(`Congratulations!! ${name} has lots of love to give and will make a great pet in your home.`)
+        //             navigate("/petshelter");
+        //         })
+        //         .catch((err) => {
+        //             console.log(err.response.data)
+        //             setGetErrors(err.response.data); 
+        //         })
+        // }
 
     return (
         <div>
+
             {/* page load ternary */}
             { name.length === 0  ? 
             <div className="Background">
@@ -66,6 +71,8 @@ const Detail = () => {
                 </Link>
             </div>
                 : //ternary for bad data flow //
+
+
             // start content //
             <div>
                 <div style={{ minHeight:"61vh"}}>
@@ -73,50 +80,87 @@ const Detail = () => {
                     {/* top ribbon */}
                     <div style={{display:"flex",justifyContent:"space-evenly", alignItems:"center", height:"15%"}}>
                         <h1 style={{color:"#073DAA"}}>Details about : {name}</h1>
-                        <div style={{marginTop:"10px"}}>
-                        <Link to="/" style={{color: "#073DAA ",fontWeight:"800", textDecoration:"underline"}}> Back To Shelter</Link>
-                            <div className="Deets" style={{marginTop:"20px"}} >
-                                <button className='DeleteButton' style={{color:"#073DAA", fontWeight:800, padding:"2px"}} onClick={(e) =>{adoptPet(id)}}>
-                                    <img  
-                                        style={{width:"55px",height:"50px",boxShadow:"0 8px 12px 0 rgba(0, 0, 0, 0.80)" }} 
-                                        src="https://thumbs.dreamstime.com/b/vector-flat-cartoon-illustration-icon-design-adopt-me-dont-buy-dog-cat-pet-adoption-puppy-pooch-kitty-cat-looking-up-to-red-heart-99463424.jpg" 
-                                        alt="Adopt Pet" 
-                                    />
-                                    <br />ADOPT ME! 
-                                </button>
+
+                        <div>
+                            <Link to="/" style={{color: "#073DAA ",fontWeight:"800", textDecoration:"underline"}}> Back To Shelter</Link>
+                            <div className="Flex">
+                                <Link to="/petshelter" style={{color: "#073DAA ",fontWeight:"800", textDecoration:"underline",width:"65px",margin:"10px"}}>All Pets</Link>
+                                <Link to="/petshelter/sort/dogs" style={{color: "#073DAA ",fontWeight:"800", textDecoration:"underline",margin:"10px"}}>Dogs</Link>
+                                <Link to="/petshelter/sort/cats" style={{color: "#073DAA ",fontWeight:"800", textDecoration:"underline",margin:"10px"}}>Cats</Link>
                             </div>
                         </div>
+                        
                     </div>
                     {/* results container */}
-                    <div style={{margin:"auto", marginTop:"30px",padding:"0 10%", minHeight:"35%", width:"40%",backgroundImage:"url(https://media.istockphoto.com/id/1064117208/vector/animal-paw-prints-on-blue-winter-background.jpg?s=612x612&w=0&k=20&c=SxtfxxqqSEHv8ms2EhMAvAuYZGFdQtqbcUOY4D3JIaU=)",backgroundSize:"cover", border:"2px solid white", borderRadius:"15px"}}>
-                        <div style={{display:"flex", justifyContent:"space-between",marginTop:"0px"}}>
-                            <p style={{color:"#073DAA", textDecoration:"underline", margin:0}}>Type</p>
-                            <p style={{margin:"3px"}}>{type}</p>
-                        </div>
-                        <hr />
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <p style={{color:"#073DAA", textDecoration:"underline", margin:0}}>Description</p>
-                            <p style={{margin:"3px"}}>{description}</p>
-                        </div>
-                        <hr />
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <p style={{color:"#073DAA", textDecoration:"underline", margin:0}}>Age</p>
-                            <p style={{margin:"3px"}}>{age}</p>
-                        </div>
-                        <hr />
-                        <div style={{display:"flex", justifyContent:"space-between"}}>
-                            <p style={{color:"#073DAA", textDecoration:"underline", margin:0}}>Skills</p>
-                            <div>
-                                <p style={{margin:"3px"}}>{skillOne}</p>
-                                <p style={{margin:"3px"}}>{skillTwo}</p>
-                                <p style={{margin:"3px"}}>{skillThree}</p>
+                    <div className="DetailContainer" style={{display:"flex", justifyContent:"space-evenly",flexWrap:"wrap", alignItems:"center",margin:"20px auto",padding:"2%", minHeight:"35%", width:"70%", minWidth:"370px",backgroundImage:"url(https://media.istockphoto.com/id/1064117208/vector/animal-paw-prints-on-blue-winter-background.jpg?s=612x612&w=0&k=20&c=SxtfxxqqSEHv8ms2EhMAvAuYZGFdQtqbcUOY4D3JIaU=)",backgroundSize:"cover", border:"2px solid white", borderRadius:"15px"}}>
+                        <img style={{width:"350px", borderRadius:"10px"}} src={petImage} alt="" />
+
+
+                        {/* <div className="Deets" style={{marginTop:"20px"}} > */}
+                            {/* THIS IS A DELETE BUTTON!!!! */}
+                            {/* <button className='DeleteButton' style={{color:"#073DAA", fontWeight:800, padding:"2px"}} onClick={(e) =>{adoptPet(id)}}>
+                                <img  
+                                    style={{width:"55px",height:"50px",boxShadow:"0 8px 12px 0 rgba(0, 0, 0, 0.80)" }} 
+                                    src="https://thumbs.dreamstime.com/b/vector-flat-cartoon-illustration-icon-design-adopt-me-dont-buy-dog-cat-pet-adoption-puppy-pooch-kitty-cat-looking-up-to-red-heart-99463424.jpg" 
+                                    alt="Adopt Pet" 
+                                />
+                                <br />ADOPT ME! 
+                            </button> */}
+                        {/* </div> */}
+
+
+                        <div style={{width:"60%",minWidth:"350px", padding:"2%"}} className="details">
+
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px",marginTop:"0px"}}>
+                                <p style={{color:"#073DAA", margin:0}}>Type</p>
+                                <p  style={{width:"250px", textAlign:"end"}}>{type}</p>
                             </div>
-                        </div>
-                        <div style={{display:"flex",alignItems:"center", justifyContent:"space-evenly" }}>
+
+                            <hr />
+                            
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{color:"#073DAA", margin:0}}>Description</p>
+                                <p  style={{width:"250px", textAlign:"end"}}>{description}</p>
+                            </div>
+                            
+                            <hr />
+                            
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{color:"#073DAA", margin:0}}>Age</p>
+                                <p  style={{width:"250px", textAlign:"end"}}>{age}</p>
+                            </div>
+                            
+                            <hr />
+                            
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{color:"#073DAA", margin:0}}>Notes</p>
+                                <div>
+                                    <p style={{width:"250px", textAlign:"start"}}>{notes}</p>
+                                </div>
+                            
+                            </div>
+                            
+                            <div>
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{textAlign:"start" }}>Fixed?</p>
+                                <p style={{width:"250px", textAlign:"end"}}>{fixed ? "Yes":"No"}</p>
+                            </div>
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{textAlign:"start" }}>Friendly to other animals?</p>
+                                <p style={{width:"250px", textAlign:"end"}}>{animalFriendly ? "Yes":"No"}</p>
+                            </div>
+                            <div style={{display:"flex",justifyContent:"space-between",padding:"10px"}}>
+                                <p style={{textAlign:"start" }}>Plays well with childern?</p>
+                                <p style={{width:"250px", textAlign:"end"}}>{childFriendly ? "Yes":"No"}</p>
+                            </div>
+
+                            
+                            </div>
                         </div>
                     </div>
                 </div>
                 <AdoptForm petName= {name} petId={id} />
+                <br />
             </div>    
             }
         </div>    
