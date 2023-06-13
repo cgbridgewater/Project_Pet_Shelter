5822087a690e5b2414107a75519e97420a15b5fe
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-12 23:38:44 -0700

    first commit

---
+++ client/src/components/CardFlip.jsx
@@ -0,0 +1,110 @@
+
+
+const CardFlip = () => {
+
+    return(
+    <div className="Background">
+
+
+        
+<div className="CardContent">
+
+
+
+    <a className="Card" href="#!">
+    <div className="CardFront" style={{backgroundImage:"url(//source.unsplash.com/300x401)"}}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="CardBack">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="CardButton">Click Here</button>
+      </div>
+    </div></a>
+
+{/* 
+
+    <a className="card" href="#!">
+    <div className="front" style="background-image: url(//source.unsplash.com/300x402)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="button">Click Here</button>
+      </div>
+    </div></a>
+
+
+
+    <a className="card" href="#!">
+    <div className="front" style="background-image: url(//source.unsplash.com/300x403)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="button">Click Here</button>
+      </div>
+    </div></a>
+
+
+
+    <a className="card" href="#!">
+    <div className="front" style="background-image: url(//source.unsplash.com/300x404)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="button">Click Here</button>
+      </div>
+    </div></a>
+
+
+
+    <a className="card" href="#!">
+    <div className="front" style="background-image: url(//source.unsplash.com/300x405)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="button">Click Here</button>
+      </div>
+    </div></a>
+
+
+
+    <a className="card" href="#!">
+    <div className="front" style="background-image: url(//source.unsplash.com/300x406)">
+      <p>Lorem ipsum dolor sit amet consectetur adipisi.</p>
+    </div>
+    <div className="back">
+      <div>
+        <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p>
+        <button className="button">Click Here</button>
+      </div>
+    </div></a> */}
+
+
+</div>
+
+
+
+
+
+
+
+
+    </div>
+    )
+}
+
+export default CardFlip
