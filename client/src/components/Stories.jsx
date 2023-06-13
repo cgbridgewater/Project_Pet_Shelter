e9a8a34c1bf7fba59a1398f46c77eda5302f9e2f
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-13 00:46:41 -0700

    add card flip and remove placeholder

--- client/src/components/Stories.jsx
+++ client/src/components/Stories.jsx
@@ -18,29 +18,7 @@ const Stories = () => {
                 <span style={{fontSize:"200%", fontWeight: 700, lineHeight:"80%", color:"#073DAA"}}>T</span>
                 he Coding Dojo Pet Shelter sees some incredibly challenging pet rescue cases.  Fortunately, we see very happy endings, too! Here are a few stories of the animal rescues we receive and a glimpse at how these special pets have touched our hearts.
             </p>
-
-
-
-            {/* REMOVE AND REPLACE WITH CONTENT!! */}
-            <p>Heart Warming</p>
-            <p>Content</p>
-            <p>and Images</p>
-            <p>Coming</p>
-            <p>Soon!!!</p>
-
-            <br />
-                <img style={{margin:"2% auto", width:"30%", height:"15%", minWidth:"325px", minHeight:"163px"}} src="https://png.pngitem.com/pimgs/s/46-464912_site-under-construction-hd-png-download.png" alt="Under Construction" />
-            <br />
-            {/* END REMOVE AND REPLACE WITH CONTENT!! */}
-
             <CardFlip/>
-
-            {/* donate button */}
-            {/* <a href="https://oregonhumane.tfaforms.net/23">
-                <img className='donate' style={{height:"75px", width:"172px", borderRadius:"5px", border: "1px solid #073DAA", marginTop:"2%"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVMMYCnUQ3zRjA9N-fsTSZ1NcTzglTZUzUxyKBtTc3zU-BCcTTukP2F9VrkVOoHubw4p4&usqp=CAU" alt="donate button"/>
-            </a> */}
-            {/* donate button */}
-            
             <br />
             <Link to="/" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline", marginRight:"50px"}}>Return To Shelter Home</Link>
 
