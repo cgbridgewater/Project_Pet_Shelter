49bc02a776b06622ee3385bae8f60c7fb4a0de6f
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-12 20:43:31 -0700

    add style link

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+<link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
