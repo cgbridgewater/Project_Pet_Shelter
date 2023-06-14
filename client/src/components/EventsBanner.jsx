5caff22cc4046f8bdd8b6df244773b6357a05e0a
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-14 02:34:58 -0700

    first commit

---
+++ client/src/components/EventsBanner.jsx
@@ -0,0 +1,49 @@
+// import {useEffect} from 'react';
+
+const EventsBanner = () => {
+
+    // useEffect(() => {
+
+    // },[])
+
+    
+    let spans = document.querySelectorAll('.word span');
+    spans.forEach((span, idx) => {
+        span.addEventListener('click', (e) => {
+            e.target.classList.add('active');
+        });
+        span.addEventListener('animationend', (e) => {
+            e.target.classList.remove('active');
+        });
+        
+        // Initial animation
+        setTimeout(() => {
+            span.classList.add('active');
+        }, 750 * (idx+1))
+    });
+
+    return(
+        <div class="FlierBanner">
+            <div class="word">
+                <span>U</span>
+                <span>P</span>
+                <span>C</span>
+                <span>O</span>
+                <span>M</span>
+                <span>I</span>
+                <span>N</span>
+                <span>G</span>
+                <br className="mobile-only"  />
+                <span>&nbsp;</span>
+                <span>E</span>
+                <span>V</span>
+                <span>E</span>
+                <span>N</span>
+                <span>T</span>
+                <span>S</span>
+            </div>
+        </div>
+    )
+}
+
+export default EventsBanner
---
+++ client/src/components/EventsViewAll.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import EventsBanner from './EventsBanner';
+
+
+const current = new Date();
+const today = `${current.getFullYear()}-${String(current.getMonth() + 1).padStart(2, '0')}-${current.getDate()}`;
+
+const EventsViewAll = () => {
+    
+    console.log("current",current)
+    console.log("events today",today)
+    // scroll to top fix
+    useEffect(() => {
+        window.scrollTo(0,0)
+    },[])
+    
+    const [ event, setEvent ] = useState([])
+    const [ eventSort, setEventSort ] = useState("NONE") 
+    const [getEventErrors, setGetEventErrors] = useState({});
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/events/" + today)
+        .then((res) => {
+            console.log(res.data);
+            setEvent(res.data);
+            setGetEventErrors("")
+        })
+        .catch((err) => {
+            console.log(err.response.data.path)
+            setGetEventErrors(err.response.data.path); //Set Errors
+        })
+    }, [eventSort])
+
+    return(
+        <div className="Background">
+            {/* Page Banner */}
+            <EventsBanner/>
+
+            {/* Container */}
+            <div className='FlierBox'>
+                {/* mapping and sorting */}
+                {event.length > 0 &&[...event]
+                .sort( (a,b) => a.date > b.date ? 1 : -1)
+                .map((event, index) => {
+                    return(
+                        <div className='FlierContainer'key={event._id}>
+                            <div className='FlierFlex'>
+                                <img width="325px" src={event.flierImage} alt="img" />
+                                <div className='FlierInfo'>
+                                    <h3>{event.title} on {event.date}</h3>
+                                    <hr className='FlierDivide' />
+                                    <br />
+                                    <h4>{event.description}</h4>
+                                </div>
+                            </div>
+                            <hr />
+                        </div>
+                        )
+                    })}
+                <br />
+                <Link to="/" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline"}}>Go To Shelter Home</Link>
+            </div>
+        </div>
+    )
+}
+
+export default EventsViewAll
