2dac26a46d5e7347bd25a1ce8e32e5c3d7d5b33d
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-14 20:53:00 -0700

    adding basic layout

--- client/src/components/AboutMe.jsx
+++ client/src/components/AboutMe.jsx
@@ -4,8 +4,23 @@ const AboutMe = () => {
 
     return(
         <div className="Background">
-                <h3 style={{color:"#073DAA", fontWeight:700, fontSize:"20px", marginTop:"20"}}>A little something about the builder</h3>            <br/>
-                
+            <div className="PageContainer" style={{width:"60%", margin:"15px auto"}}>
+
+                <p style={{textAlign:"start", fontSize:"18px", fontWeight:800, color:"#073DAA"}}>
+                    <span style={{fontSize:" 200%", fontWeight: 700, lineHeight:"80%", color:"#073DAA"}}>W</span>
+                        elcome to my mock pet shelter site. This is a single page app utilizing the MERN stack, and has a handfull of features built into it:
+                </p>
+                <ul style={{width:"80%", margin:"15px auto", textAlign:"start"}}>
+                    <li>Full CRUD functionality on both the pets up for adoptoin and the event fliers</li>
+                    <li>Admin login/registration pages are validated by Bcrypt and using hashed passwords for added security</li>
+                    <li>Update and Delete functions are protected by limiting access with JWT tokens via the admin pages</li>
+                    <li>Integration of location mapping API from Google's Cloud Services</li>
+                    <li>On page sorting for pets and events with an array of sorting types</li>
+                    <li>Backend Data filtering for event fliers to ensure old data is not rendered</li>
+                    <li>All displays, actions and animations are built with pure CSS and with no added libraries</li>
+                    <li>A responsive design for friendly viewing across multiple devices and screen sizes, which includes phones and tablets</li>
+                </ul>
+                <br />
                 <hr />
                 
                 <h3 style={{color:"#073DAA", fontWeight:700, fontSize:"20px", marginTop:"20"}}>Check out my GitHub</h3>            <br/>
@@ -45,6 +60,7 @@ const AboutMe = () => {
     
     
     
+            </div>
     </div>
     )
 }
