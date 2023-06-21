d8f5e4ada90f47de2067f384d5170878da1c98e7
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-20 17:35:41 -0700

    add adoption and clean up

--- client/src/components/AdminViewAll.jsx
+++ client/src/components/AdminViewAll.jsx
@@ -2,35 +2,33 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-
 // Pet Sorting
 const petSortType = { 
     NONE: (a,b) => a.createdAt > b.createdAt ? -1 : 1,
     TYPEA2Z: (a,b) => a.type > b.type ? 1 : -1,
     TYPEZ2A: (a,b) => a.type > b.type ? -1 : 1,
-    ATOZ: (a,b) => a.name.localeCompare(b.name),                   
+    ATOZ: (a,b) => a.name.localeCompare(b.name),
     ZTOA: (a,b) => b.name.localeCompare(a.name)
 }
 
-
-// Event sorting
-const current = new Date();
-  const today = `${current.getFullYear()}-${String(current.getMonth() + 1).padStart(2, '0')}-${current.getDate()}`;
-
+// Event Sorting
 const eventSortType = { 
-    // NONE: (a,b) => a.createdAt > b.createdAt ? -1 : 1,
     NONE: (a,b) => a.date > b.date ? 1 : -1,
-    ATOZ: (a,b) => a.title.localeCompare(b.title),                   
+    ATOZ: (a,b) => a.title.localeCompare(b.title),
     ZTOA: (a,b) => b.title.localeCompare(a.title),
-    // Upcoming: (a,b)=> today > b.date ? -1 : 1,
-    // DateOld: (a,b)=> today > b.date ? 1 : -1,
+}
+
+// Adoption Sorting
+const AdoptSortType = { 
+    NONE: (a,b) => a.date > b.date ? 1 : -1,
+    ATOZ: (a,b) => a.petName.localeCompare(b.petName),
+    ZTOA: (a,b) => b.petName.localeCompare(a.petName)
 }
 
 
 const AdminViewAll = () => {
-console.log("current",current)
-console.log("today",today)
-    // scroll to top fix
+
+    // scroll to top
     useEffect(() => {
         window.scrollTo(0,0)
     },[])
@@ -41,6 +39,9 @@ console.log("today",today)
     const [ event, setEvent ] = useState([])
     const [ eventSort, setEventSort ] = useState("NONE") 
     const [getEventErrors, setGetEventErrors] = useState({});
+    const [ adopt, setAdopt ] = useState([])
+    const [ adoptSort, setAdoptSort ] = useState("NONE") 
+    const [getAdoptErrors, setGetAdoptErrors] = useState({});
 
     useEffect(() => {
         axios.get("http://localhost:8000/api/events")
@@ -68,32 +69,44 @@ console.log("today",today)
         })
     }, [petSort])
 
-
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/adopt")
+        .then((res) => {
+            console.log(res.data);
+            setAdopt(res.data);
+            setGetAdoptErrors("")
+        })
+        .catch((err) => {
+            console.log(err.response.data.path)
+            setGetAdoptErrors(err.response.data.path); //Set Errors
+        })
+    }, [adoptSort])
 
     return(
         <div className="Background">
-
-            {/* LEFT TABLE */}
             <div className='PetListTable'>
+                {/* Top Row */}
                 <div style={{padding:"0 10%",display:"flex",justifyContent:"space-evenly", alignItems:"center", flexWrap:"wrap"}}>
                     <h1 style={{fontSize:"40px",color:"red"}}> ADMIN PAGE</h1>
-                    <Link to="/admin/new/pet" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline"}}>Add a pet to the shelter</Link>
-                    <Link to="/admin/new/event" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline"}}>Add a new event for the shelter</Link>
                 </div>
                 <br />
 
+                {/* tables contaner */}
                 <div className='TablesContainer' style={{display:"flex", justifyContent:"space-evenly", flexWrap:"wrap"}}>
 
-
-                    {/* pet table container */}
+                    {/* LEFT TABLE */}
+                    {/* pet table */}
                     <div className='TableBox'>
-
+                        <div className="TitleBox">
+                            <h1><em>PETS FOR ADOPTION</em></h1>
+                            <Link to="/admin/new/pet" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline",color:"white"}}>Add a pet to the shelter</Link>
+                        </div>
                         <table className='PetTable'>
                             {/* sorting menu */}
                             <div style={{display:"flex", justifyContent:"center", flexDirection:"column", width:"150px", border:"3px solid #073DAA"}} >
                                 {/* <label style={{fontSize:"18px", fontWeight:800, color:"#073DAA",backgroundColor:"white"}} htmlFor="">Sort</label> */}
                                 <select value={petSort} onChange={(e) => setPetSort(e.target.value)} style={{textAlign:"center",border:"3px solid white", fontSize:"18px", color:"white",backgroundColor:"#073DAA",boxShadow:"0 8px 12px 0 rgba(0, 0, 0, 0.80)"}}>
-                                    <option value="NONE">Newest Added</option>
+                                    <option value="NONE">ALL</option>
                                     <option value="ATOZ">A to Z</option>
                                     <option value="ZTOA">Z to A</option>
                                     <option value="TYPEA2Z">Type A to Z</option>
@@ -116,9 +129,9 @@ console.log("today",today)
                                             <tbody key={pet._id}>
                                 <tr>
                                     <td>
-                                        <Link className='TableLink' to={"/admin/edit/" +pet._id}>{pet.name}</Link>
+                                        <Link className='TableLink' to={"/admin/edit/pet/" +pet._id}>{pet.name}</Link>
                                     </td>
-                                    <td style={{color:"white", fontSize:"16px"}}>
+                                    <td style={{color:"white", fontSize:"16px", textAlign:"center"}}>
                                         {pet.type}
                                     </td>
                                 </tr>
@@ -130,27 +143,28 @@ console.log("today",today)
 
 
 
-                {/* RIGHT TABLE */}
-                
-                    {/* table container */}
-                    <div className='TableBox'>
+                {/* Center TABLE */}
+                <div className='TableBox'>
+                    <div className="TitleBox">
+                        <h1><em>EVENTS LIST</em></h1>
+                        <Link className="Add" to="/admin/new/event" style={{fontSize:"20px", fontWeight:"700", textDecoration:"underline",color:"white"}}>Add a new event for the shelter</Link>
+                    </div>
+
                         <table className='EventTable'>
                             {/* sorting menu */}
                             <div style={{display:"flex", justifyContent:"center", flexDirection:"column", width:"150px", border:"3px solid #073DAA"}} >
                                 {/* <label style={{fontSize:"18px", fontWeight:800, color:"#073DAA",backgroundColor:"white"}} htmlFor="">Sort</label> */}
                                 <select value={eventSort} onChange={(e) => setEventSort(e.target.value)} style={{textAlign:"center",border:"3px solid white", fontSize:"18px", color:"white",backgroundColor:"#073DAA",boxShadow:"0 8px 12px 0 rgba(0, 0, 0, 0.80)"}}>
-                                    <option value="NONE">ALL Events</option>
+                                    <option value="NONE">ALL</option>
                                     <option value="ATOZ">A to Z</option>
                                     <option value="ZTOA">Z to A</option>
-                                    {/* <option value="Past Events">Past Events</option>
-                                    <option value="Upcoming Events">Upcoming Events</option> */}
                                 </select>
                             </div>
                             {/* table header */}
                             <thead>
                                 <tr>
                                     <th>Event Name</th>
-                                    <th>Actions</th>
+                                    <th>Event Date</th>
                                 </tr>
                             </thead>
                                 {/* mapping and sorting */}
@@ -160,19 +174,72 @@ console.log("today",today)
                                         return(
                                             // table body // 
                                             <tbody key={event._id}>
+                                                <tr>
+                                                    {/* Title */}
+                                                    <td>
+                                                        <Link  className='TableLink' to={"/admin/edit/event/" +event._id}>{event.title}</Link>
+                                                    </td>
+                                                    {/* Date */}
+                                                    <td style={{color:"white", fontSize:"16px", textAlign:"center"}}>
+                                                        {event.date}
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        )})
+                                    }  {/* end mapping */}
+                        </table>
+                    </div>
+
+                {/* RIGHT TABLE */}
+                    {/* table container */}
+                    <div className='TableBox'>
+                        <div className="TitleBox">
+                        <h1><em>ADOPTION REQUESTS</em></h1>
+                        <h3>Generated user side input</h3>
+                        </div>
+                        <table className='AdoptTable'>
+                            {/* sorting menu */}
+                            <div style={{display:"flex", justifyContent:"center", flexDirection:"column", width:"150px", border:"3px solid #073DAA"}} >
+                                {/* <label style={{fontSize:"18px", fontWeight:800, color:"#073DAA",backgroundColor:"white"}} htmlFor="">Sort</label> */}
+                                <select value={adoptSort} onChange={(e) => setAdoptSort(e.target.value)} style={{textAlign:"center",border:"3px solid white", fontSize:"18px", color:"white",backgroundColor:"#073DAA",boxShadow:"0 8px 12px 0 rgba(0, 0, 0, 0.80)"}}>
+                                    <option value="NONE">ALL</option>
+                                    <option value="ATOZ">By Pet - A to Z</option>
+                                    <option value="ZTOA">By Pet - Z to A</option>
+                                </select>
+                            </div>
+                            {/* table header */}
+                            <thead>
                                 <tr>
-                                    {/* Title */}
-                                    <td>
-                                        <Link  className='TableLink' to={"/admin/event/edit/" +event._id}>{event.title}</Link>
-                                    </td>
-                                    {/* Date */}
-                                    <td style={{color:"white", fontSize:"16px"}}>
-                                        {event.date}
-                                    </td>
+                                    <th>Contact Email </th>
+                                    <th>Pet Name</th>
                                 </tr>
-                            </tbody>
-                            )
-                        })}
+                            </thead>
+                                {/* mapping and sorting */}
+                                {adopt.length > 0 &&[...adopt]
+                                    .sort(AdoptSortType[adoptSort])
+                                    .map((adopt, index) => {
+                                        return(
+                                            // table body // 
+                                            <tbody key={adopt._id}>
+                                                <tr>
+                                                    <td>
+                                                        <Link 
+                                                            style={{textAlign:"end"}}
+                                                            className='TableLink' 
+                                                            to={"/admin/adopt/" +adopt._id}
+                                                        >
+                                                            {adopt.email}
+                                                        </Link>
+                                                    </td>
+                                                    {/* Pet Name */}
+                                                    <td style={{color:"white", fontSize:"16px", textAlign:"center"}}>
+                                                        {adopt.petName}
+                                                    </td>
+                                                    {/* Contact */}
+                                                </tr>
+                                            </tbody>
+                                            )})
+                                        }  {/* end mapping */}
                         </table>
                     </div>
                 </div>
