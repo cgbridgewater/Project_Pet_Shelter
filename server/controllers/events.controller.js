7a10deb587f395d3e99f3aadd0297fcda664cdee
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-20 17:34:42 -0700

    update controller actions

--- server/controllers/events.controller.js
+++ server/controllers/events.controller.js
@@ -1,6 +1,5 @@
 const Event = require('../models/event.model')
-const today = Date("<YYYY-mm-dd>").toString
-
+// const today = Date("<YYYY-mm-dd>").toString
 
 module.exports.create = (req, res) => {
     Event.create(req.body)
@@ -16,19 +15,41 @@ module.exports.getAll = (req,res) => {
             console.log(result)
             res.json(result);
         })
-        .catch((err) => res.status(400).json({message: "HERE!! Something went wrong with the GetAll in Axios",error: err}))
+        .catch((err) => res.status(400).json({message: " Something went wrong with the GetAll in Axios",error: err}))
 }
 
 module.exports.sorter = (req,res) => {
     Event.find({
         date: 
         {$gt: (req.params.today)}
-         
-    // {$gt: ()}"2023-06-13"
 })
             .then(result => {
                 console.log(result)
                 res.json(result);
             })
             .catch((err) => res.status(400).json({message: "Something went wrong with the Sorter in Axios",error: err}))
+}
+
+module.exports.getOne = (req,res) => {
+    Event.findOne({_id: req.params.id})
+            .then(result => {
+                console.log(result)
+                res.json(result);
+            })
+            .catch((err) => res.status(400).json({message: "Something went wrong with FindOne in Axios",error: err}))
+}
+
+module.exports.update = (req,res) => {
+    Event.findOneAndUpdate({_id: req.params.id}, req.body, {new:true, runValidators: true})
+        .then(updated => {
+            console.log(updated)
+            res.json(updated);
+        })
+        .catch((err) => res.status(400).json({message: "Something went wrong with FindOneAndUpdate in Axios",error: err}))
+}
+
+module.exports.delete = (req,res) => {
+    Event.deleteOne({ _id: req.params.id})
+        .then(deleteConfirmation => res.json(deleteConfirmation))
+        .catch((err) => res.status(400).json({message: "Something went wrong with Delete in Axios",error: err}))
 }
