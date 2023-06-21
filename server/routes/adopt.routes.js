c6e0591d386ceaa3b9323061e0b372ec31fbfd1d
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-20 19:31:47 -0700

    typo

--- server/routes/adopt.routes.js
+++ server/routes/adopt.routes.js
@@ -2,8 +2,8 @@ const AdoptController = require('../controllers/adopt.controller');
 
 module.exports = (app) => {
     app.get('/api/adopt', AdoptController.getAll);
+    app.post('/api/adopt', AdoptController.create);
     app.get('/api/adopt/:id', AdoptController.getOne);
-    app.post('/api/adopt/adopt', AdoptController.create);
     app.delete('/api/adopt/:id', AdoptController.delete);
     // app.put('/api/adopt/:id', AdoptController.update); // just in case!
     // app.get('/api/adopt/sort/:type', AdoptController.sorter); // just in case!
