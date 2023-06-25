1399d362b0a17e6f41b82722de2bc3beb6d17524
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-25 16:45:03 -0700

    add fees page

--- client/src/App.js
+++ client/src/App.js
@@ -44,6 +44,7 @@ import OurStory from './components/OurStory';
 import Login from './components/AdminLogin';
 import Register from './components/AdminRegister';
 import AdminLogout from './components/AdminLogout';
+import Fees from './components/Fees';
 
 function App() {
 
@@ -72,6 +73,7 @@ function App() {
             <Route path="/petshelter" element={<PetShow/>}/>
             <Route path="/petshelter/aboutme" element={<AboutMe/>}/>
             <Route path="/petshelter/adopting"element={<Adopting/>}/>
+            <Route path="/petshelter/fees"element={<Fees/>}/>
             <Route path="/petshelter/sort/:type" element={<TypeSort/>}/>
             <Route path="/petshelter/liveFeed" element={<LiveFeed/>}/>
             <Route path="/petshelter/ourstory" element={<OurStory/>}/>
