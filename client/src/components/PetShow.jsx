6416afec7088e52d9e0d7f004004150889893e98
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-25 13:05:19 -0700

    cleaning

--- client/src/components/PetShow.jsx
+++ client/src/components/PetShow.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-// import defaultPetImage from '../assets/images/dogCatDefault.png'
 
     // sorting
     const sortType = { 
