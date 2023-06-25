87237ef068d7d5e3ab421d92bfd7ed24d10df0bc
Chris Bridgewater <cgbridgewater@outlook.com>
2023-06-24 17:32:57 -0700

    add function to login reg

--- client/src/components/AdminLogin.jsx
+++ client/src/components/AdminLogin.jsx
@@ -1,31 +1,116 @@
-const Login = () => {
+import { useState } from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+
+const Login = (props) => {
+
+    const [regState, setRegState] = useState ({
+        register: {
+            firstName: "",
+            lastName: "",
+            email:"",
+            password:"",
+            confirmPassword:""
+        }
+    });
+
+    const {register} = regState
+    const handleRegInputs =(e) => {
+        props.setAuthorized("");
+        setRegState({...regState, register: {...regState.register,[ e.target.name]: e.target.value}})
+    }
+    const handleRegistration = (e) =>{
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/register", register, {withCredentials: true})
+            .then(res => {console.log(res)
+                navigate("/users")
+            })
+            .catch(err => console.log(err))
+    }
+
+
+
+    const [loginState, setLoginState] = useState ({
+        login:{
+            email:"",
+            password:""
+        }
+    })
+
+    const {login} = loginState;
+    const navigate = useNavigate()
+
+    const handleLoginInputs = (e) => {
+        PaymentResponse.setAuthorized("");
+        setLoginState({...loginState, login: {...loginState.login, [e.target.name]: e.target.value}})
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        axios.post("http://localhost:8000/api/login", login, {withCredentials:true})
+        .then(res => { console.log(res)
+            navigate("/admin/viewall")
+        })
+        .catch(err => console.log(err))
+    }
+
+
 
     return(
         <div className="background">
-            <div class="container">
+            <div className="container">
                 <input type="radio" name="switch" id="switch_register" />
-                <label class="l_switch" for="switch_register">Need To Sign Up? </label>
-                <form class="form">
+                <label className="l_switch" htmlFor="switch_register">Need To Sign Up? </label>
+                <form onSubmit={handleRegistration} className="form">
                     <div>
-                    <h2>Register</h2>
-                    <label>Email</label>
-                    <input placeholder="email address"/>
-                    <label>Password</label>
-                    <input type="password" placeholder="passwords"/>
-                    <label>Verify Password</label>
-                    <input type="password" placeholder="password"/>
-                    <button>Sign Up!</button>
+                    <h2>Sign Up</h2>
+                    {/* email signup */}
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        name="email" 
+                        placeholder="email address"
+                        onChange={handleRegInputs}
+                    />
+                    {/* password signup */}
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        placeholder="password"
+                        onChange={handleRegInputs}
+                    />
+                    {/* password signup */}
+                    <label htmlFor="verPass">Verify Password</label>
+                    <input 
+                    name="verPass" 
+                    type="password" 
+                    placeholder="password"
+                    onChange={handleRegInputs}
+                    />
+                    {/* signup button */}
+                    <button >Sign Up!</button>
                     </div>
                 </form>
-                <input type="radio" name="switch" id="switch_login" checked/>
-                <label class="l_switch" for="switch_login">Already A Member?</label>
-                <form class="form">
+                <input type="radio" name="switch" id="switch_login" defaultChecked/>
+                <label className="l_switch" htmlFor="switch_login">Already A Member?</label>
+                <form className="form" onSubmit={handleLogin}>
                     <div> 
                     <h2>Login</h2>
-                    <label>Username</label>
-                    <input placeholder="email address"/>
-                    <label>Password</label>
-                    <input type="password" placeholder="password"/>
+                    {/* email login */}
+                    <label htmlFor="username">Email</label>
+                    <input
+                        name="email" 
+                        placeholder="email address"
+                        onChange={handleLoginInputs}
+                    />
+                    {/* password login */}
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        name="password" 
+                        type="password" 
+                        placeholder="password"
+                        onChange={handleLoginInputs}    
+                    />
+                    {/* login button */}
                     <button>Log In!</button>
                     </div>
                 </form>
